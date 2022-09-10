14e9ce30769f9cd163e0486dede3920e7c57808d
Mukund Khunt <64716112+Mukundkhunt@users.noreply.github.com>
2022-09-10 14:28:46 +0530

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { IgrLegendModule, IgrCategoryChartModule } from 'igniteui-react-charts';
+import { IgrLegend, IgrCategoryChart } from 'igniteui-react-charts';
+import { CountryRenewableElectricityItem, CountryRenewableElectricity } from './CountryRenewableElectricity';
+
+
+
+const mods: any[] = [
+    IgrLegendModule,
+    IgrCategoryChartModule
+];
+mods.forEach((m) => m.register());
+
+export default class Sample extends React.Component<any, any> {
+    private legend: IgrLegend
+    private legendRef(r: IgrLegend) {
+        this.legend = r;
+        this.setState({});
+    }
+    private chart: IgrCategoryChart
+    private chartRef(r: IgrCategoryChart) {
+        this.chart = r;
+        this.setState({});
+    }
+
+    constructor(props: any) {
+        super(props);
+
+        this.legendRef = this.legendRef.bind(this);
+        this.chartRef = this.chartRef.bind(this);
+    }
+
+    public render(): JSX.Element {
+        return (
+        <div className="container sample">
+
+            <div className="legend-title">
+                Renewable Electricity Generated
+            </div>
+
+            <div className="legend">
+                <IgrLegend
+                    orientation="Horizontal"
+                    ref={this.legendRef}>
+                </IgrLegend>
+            </div>
+
+            <div className="container fill">
+                <IgrCategoryChart
+                    chartType="Line"
+                    yAxisLabelLeftMargin="0"
+                    yAxisTitleLeftMargin="10"
+                    yAxisTitleRightMargin="5"
+                    yAxisTitle="TWh"
+                    dataSource={this.countryRenewableElectricity}
+                    includedProperties={["year", "europe", "china", "america"]}
+                    legend={this.legend}
+                    isHorizontalZoomEnabled="false"
+                    isVerticalZoomEnabled="false"
+                    computedPlotAreaMarginMode="Series"
+                    ref={this.chartRef}>
+                </IgrCategoryChart>
+            </div>
+        </div>
+        );
+    }
+
+    private _countryRenewableElectricity: CountryRenewableElectricity = null;
+    public get countryRenewableElectricity(): CountryRenewableElectricity {
+        if (this._countryRenewableElectricity == null)
+        {
+            this._countryRenewableElectricity = new CountryRenewableElectricity();
+        }
+        return this._countryRenewableElectricity;
+    }
+    
+
+
+}
+
+
+// rendering above component in the React DOM
+ReactDOM.render(<Sample />, document.getElementById('root'));
